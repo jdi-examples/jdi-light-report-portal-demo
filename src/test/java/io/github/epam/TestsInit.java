d7a30b1936852dbc6896523d0a0c6c4e87d3d7f9
Dzmitry Danilau <Dzmitry_Danilau@epam.com>
2020-05-27 16:35:42 +0300

    remove system property from pom.xml

--- src/test/java/io/github/epam/TestsInit.java
+++ src/test/java/io/github/epam/TestsInit.java
@@ -6,14 +6,12 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import static com.epam.jdi.light.elements.composite.WebPage.openSite;
-import static com.epam.jdi.light.elements.init.PageFactory.initSite;
 import static com.epam.jdi.light.settings.WebSettings.logger;
 
 public interface TestsInit {
 
     @BeforeSuite(alwaysRun = true)
     static void setUp() {
-        initSite(StaticSite.class);
         openSite(StaticSite.class);
         logger.toLog("Run Tests");
     }
