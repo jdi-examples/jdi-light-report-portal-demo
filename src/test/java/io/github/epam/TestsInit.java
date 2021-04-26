f2dff065e602d6b03365c700bab63a6492e4fe0a
Akio Nkodia <AKIONK@ext.inditex.com>
2021-04-26 22:44:37 +0300

    Implement addTestProperty() method to add driver.remote.url property to test.properties

--- src/test/java/io/github/epam/TestsInit.java
+++ src/test/java/io/github/epam/TestsInit.java
@@ -5,23 +5,29 @@ import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.settings.JDISettings;
 import com.epam.jdi.tools.PropertyReader;
 import io.github.com.StaticSite;
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.PropertiesConfigurationLayout;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.apache.commons.lang3.ObjectUtils;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
-import static com.epam.jdi.light.settings.WebSettings.jdiSetup;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Objects;
+
 import static com.epam.jdi.light.settings.WebSettings.logger;
 import static java.lang.System.getProperty;
 
 public interface TestsInit {
 
-    static void setRemoteWebDriverIfRequired() {
+    static void setRemoteWebDriverIfRequired() throws IOException, ConfigurationException {
         String remoteUrl = getProperty("webdriver.remote.url");
         if (remoteUrl != null) {
             logger.toLog("Set remote driver options");
-            PropertyReader.loadProperties().setProperty("driver.remote.url", remoteUrl);
-          /*  JDISettings.DRIVER.remoteUrl = remoteUrl;
-            JDISettings.DRIVER.remoteRun = true;*/
+            addTestProperty("driver.remote.url", remoteUrl);
             JDISettings.DRIVER.capabilities.chrome.put("w3c", "true");
             JDISettings.DRIVER.capabilities.chrome.put("platformName",
                     ObjectUtils.defaultIfNull(getProperty("webdriver.platform.name"), "Linux"));
@@ -30,14 +36,17 @@ public interface TestsInit {
             if (buildTag != null) {
                 JDISettings.DRIVER.capabilities.chrome.put("build", "build: " + buildTag);
             }
-            jdiSetup();
         }
     }
 
     @BeforeSuite(alwaysRun = true)
     static void setUp() {
         JDISettings.DRIVER.domain = PropertyReader.getProperties("test.properties").getProperty("domain");
-        setRemoteWebDriverIfRequired();
+        try {
+            setRemoteWebDriverIfRequired();
+        } catch (ConfigurationException | IOException e) {
+            logger.error("Error during setting of remote web driver properties");
+        }
         WebPage.openSite(StaticSite.class);
         logger.toLog("Run Tests");
     }
@@ -45,5 +54,17 @@ public interface TestsInit {
     @AfterSuite(alwaysRun = true)
     static void tearDown() {
         WebDriverFactory.quit();
+    }
+
+    static void addTestProperty(String key, String value) throws IOException, ConfigurationException {
+        PropertiesConfiguration config = new PropertiesConfiguration();
+        PropertiesConfigurationLayout layout = new PropertiesConfigurationLayout();
+        config.setLayout(layout);
+        File propertiesFile = new File(Objects.requireNonNull(
+                TestsInit.class.getClassLoader().getResource(JDISettings.COMMON.testPropertiesPath)).getFile());
+        layout.load(config, new FileReader(propertiesFile));
+
+        config.setProperty(key, value);
+        layout.save(config, new FileWriter(propertiesFile));
     }
 }
